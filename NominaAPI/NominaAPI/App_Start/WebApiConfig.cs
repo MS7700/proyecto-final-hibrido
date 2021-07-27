a39f5c370301d4e55b816cb3ae2f39fae5ad09d7
MS7700 <50929878+MS7700@users.noreply.github.com>
2021-07-27 15:32:54 -0400

    NominaAPI added

---
+++ NominaAPI/NominaAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+//using ProductService.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+
+namespace NominaAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+            // New code:
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            //builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
